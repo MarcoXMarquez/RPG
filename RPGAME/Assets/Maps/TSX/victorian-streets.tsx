17bf2bee2edfb1d7c9fdc1948162d5efe68fa78d
Marco <marcomarquezherrera@gmail.com>
2025-10-19 22:42:09 -0500

    feat(map,character): update map and character animations and collisions

---
+++ RPGAME/Assets/Maps/TSX/victorian-streets.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="victorian-streets" tilewidth="16" tileheight="16" tilecount="6144" columns="32">
+ <image source="C:\Users\marco\2025 - B\CONSTRUCCION DE SOFTWARE\LABORATORIO\RPGAME\Assets\Maps\Textures\victorian-streets.png" width="512" height="3072"/>
+</tileset>
--- RPGAME/Assets/Scripts/AreaEntrance.cs
+++ RPGAME/Assets/Scripts/AreaEntrance.cs
@@ -1,21 +1,31 @@
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
 public class AreaEntrance : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     public string transitionName;
-    void Start()
-    {
-        if(transitionName == PlayerController2D.instance.areaTransitionName)
-        {
-            PlayerController2D.instance.transform.position = transform.position;
-        }
-        UIFade.instance.FadeFromBlack();
-    }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator Start()
     {
-        
+        // Espera un frame a que PlayerController2D despierte
+        yield return null;
+
+        if (PlayerController2D.instance != null)
+        {
+            if (transitionName == PlayerController2D.instance.areaTransitionName)
+            {
+                PlayerController2D.instance.transform.position = transform.position;
+            }
+        }
+        else
+        {
+            Debug.LogWarning("⚠️ PlayerController2D.instance es nulo en AreaEntrance");
+        }
+
+        // También protege UIFade por si acaso
+        if (UIFade.instance != null)
+        {
+            UIFade.instance.FadeFromBlack();
+        }
     }
 }
